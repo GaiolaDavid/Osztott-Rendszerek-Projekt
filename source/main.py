e71eb9e88431ef90eb5f3996c1c273a3ddf33393
norbi <sznt.norbi@gmail.com>
2025-03-12 11:21:47 +0200

    order html files

--- source/main.py
+++ source/main.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template('index.html')
+    return render_template('pages/index.html')
 
 @app.route("/form")
 def form_page():
-    return render_template('form.html')
+    return render_template('pages/form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
